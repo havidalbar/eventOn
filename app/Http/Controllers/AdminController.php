3de874a7b5fbb52dcca6b69dbdab3a665c01e235
havidalbar@gmail.com <havidalbar@gmail.com>
2019-09-22 23:54:20 +0700

    Add method according to design admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,80 @@
 
 namespace App\Http\Controllers;
 
+use App\Panitia;
+use App\User;
+use App\Pesan;
+
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    public function lihatHalamanKonfirmasiPendaftaran()
+    {
+        $panitias = Panitia::where('status', null)->get();
+        $pesanans = Pesan::where('status', 0)->get();
+        $users = array();
+        for ($i = 0; $i < count($panitias); $i++) {
+            $users[$i] = User::find($panitias[$i]->id_user);
+        }
 
+        return view('admin.konfirmasi-pendaftaran', compact('panitias', 'pesanans', 'users'));
+    }
+
+    public function terimaPanitia(Request $request)
+    {
+        $dataPanitia = Panitia::find($request->input('id'));
+        if ($dataPanitia) {
+            $dataPanitia->status = 1;
+            $dataPanitia->save();
+            return redirect()->back()->with('alert-success', 'Berhasil menerima panitia');
+        } else {
+            abort(404);
+        }
+    }
+
+    public function tolakPanitia(Request $request)
+    {
+        $dataPanitia = Panitia::find($request->input('id'));
+        if ($dataPanitia) {
+            $dataPanitia->delete();
+            return redirect()->back()->with('alert', 'Berhasil menolak panitia');
+        } else {
+            abort(404);
+        }
+    }
+    public function lihatHalamanKonfirmasiPembayaran()
+    {
+        $panitias = Panitia::where('status', null)->get();
+        $pesanans = Pesan::where('status', 0)->get();
+        $users = array();
+        for ($i = 0; $i < count($panitias); $i++) {
+            $users[$i] = User::find($panitias[$i]->id_user);
+        }
+        return view('admin.konfirmasi-pembayaran', compact('panitias', 'pesanans', 'users'));
+    }
+
+    public function terimaPembayaran(Request $request)
+    {
+        $dataPesan = Pesan::find($request->input('id'));
+        if ($dataPesan) {
+            $dataPesan->status = 1;
+            $dataPesan->save();
+            return redirect()->back()->with('alert-success', 'Berhasil menyetujui transfer');
+        } else {
+            abort(404);
+        }
+    }
+
+    public function tolakPembayaran(Request $request)
+    {
+        $dataPesan = Pesan::find($request->input('id'));
+        if ($dataPesan) {
+            $dataPesan->status = -1;
+            $dataPesan->save();
+            return redirect()->back()->with('alert-success', 'Berhasil menolak transfer');
+        }else{
+            abort(404);
+        }
+    }
 }
