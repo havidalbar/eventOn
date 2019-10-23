3d6529a4c933674ff097100c2a766fbc8cad8181
havidalbar@gmail.com <havidalbar@gmail.com>
2019-10-23 22:00:02 +0700

    cek kata

--- app/Http/Controllers/PanitiaController.php
+++ app/Http/Controllers/PanitiaController.php
@@ -150,7 +150,8 @@ class PanitiaController extends Controller
 
                         return redirect()->route('tamu.user.panitia.verif.lihat-halaman-scan-barcode')->with('alert-success', 'Absensi peserta dengan kode ' . $kode_pesanan . ' berhasil dimasukkan');
                     }
-                    return view('halamanPanitia.scanQrCode',['alert'=>'Maaf peserta dengan kode ' . $kode_pesanan . ' sudah terdaftar']);
+                    $kata = 'Maaf peserta dengan kode ' . $kode_pesanan . ' sudah terdaftar';
+                    return view('halamanPanitia.scanQrCode',['alert'=>$kata]);
                 } else {
                     return redirect()->route('tamu.user.panitia.verif.lihat-halaman-scan-barcode')->with('alert', 'peserta tidak ditemukan');
                 }
